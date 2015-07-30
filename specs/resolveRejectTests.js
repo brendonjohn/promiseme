161c9b53e8894a44585e37c0f624a3b8f9dba2ef
Jacob Duval <jladuval@gmail.com>
2015-07-30 17:00:54 +1200

    Initial Commit :D

---
+++ specs/resolveRejectTests.js
@@ -0,0 +1,8 @@
+/* globals describe, it */
+import {expect} from 'chai'
+//import PromiseMe from '../lib/'
+
+describe('When I create a new Promise', () => {
+  it('I should receive a promise object', () => {
+  })
+})
